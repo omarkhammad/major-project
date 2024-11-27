63c99e2301dbffd376d427f83b5ad3a215c9195b
omarkhammad <omar_hammad_512@yahoo.com>
2024-11-27 14:03:51 -0600

    Make Tetris grid show up

--- sketch.js
+++ sketch.js
@@ -2,9 +2,10 @@
 
 const NUMBER_OF_ROWS = 20;
 const NUMBER_OF_COLUMNS = 10;
-let gridArray;
+let tetrisArray = [];
 let squareSize;
 let xSquarePadding;
+let ySquarePadding;
 
 class Tetris {
   constructor(row, col) {
@@ -13,39 +14,69 @@ class Tetris {
     this.state = 0;
   }
 
-  display() {
+  setCoordinates() {
     this.x = this.col * squareSize + xSquarePadding;
     this.y = this.row * squareSize;
-    square(this.x, this.y);
+  }
+
+  display() {
+    square(this.x + xSquarePadding, this.y + ySquarePadding, squareSize);
   }
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createEmptySquareGrid();
   setSquareSize();
 }
 
 
 function draw() {
-  
+  for (let arr of tetrisArray) {
+    for (let newTetris of arr) {
+      newTetris.display();
+    }
+  }
+
+  // allTetris(display);
 }
 
 
 function setSquareSize() {
-  if (width / NUMBER_OF_ROWS < height / NUMBER_OF_COLUMNS) {
-    squareSize = width / NUMBER_OF_ROWS;
+  if (width / NUMBER_OF_COLUMNS < height / NUMBER_OF_ROWS) {
+    squareSize = width / NUMBER_OF_COLUMNS;
   }
   else {
-    squareSize = height / NUMBER_OF_COLUMNS;
+    squareSize = height / NUMBER_OF_ROWS;
   }
-  xSquarePadding = (NUMBER_OF_COLUMNS * squareSize - width) / 2;
+  xSquarePadding = (width - NUMBER_OF_COLUMNS * squareSize) / 4;
+  
+  ySquarePadding = (height - NUMBER_OF_ROWS * squareSize) / 4;
+  
+  for (let arr of tetrisArray) {
+    for (let newTetris of arr) {
+      newTetris.setCoordinates();
+    }
+  }
 }
 
-function createEmptySquareGrid () {
+function createEmptySquareGrid() {
   for (let y = 0; y < NUMBER_OF_ROWS; y++) {
+    let arr = [];
     for (let x = 0; x < NUMBER_OF_COLUMNS; x++) {
-      square = new Tetris(y, x);
+      let newTetris = new Tetris(y, x);
+      arr.push(newTetris);
+    }
+    tetrisArray.push(arr);
+  }
+}
+
+
+function allTetris(func) {
+  for (let arr of tetrisArray) {
+    for (let newTetris of arr) {
+      newTetris.func();
     }
   }
 }
